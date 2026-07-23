320e699f41a5409f77f278971a9de8d2c2e338a1
bibasulaim <85745563+bibasulaim@users.noreply.github.com>
2026-07-24 08:42:22 +0900

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+// Import Routes
+const authRoutes = require('./routes/auth');
+
+// Link API Routes Middleware
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
-    res.send("Server works!");
+    res.send("Zikhas Data App Server is running smoothly!");
 });
 
 app.listen(5000, () => {
-    console.log("Running on 5000");
+    console.log("Running perfectly on port 5000");
 });
