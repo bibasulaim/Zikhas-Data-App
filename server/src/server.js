5f7c54b28a2869df6ae6d13414390be7d8400e9b
bibasulaim <85745563+bibasulaim@users.noreply.github.com>
2026-07-24 08:28:19 +0900

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => res.send("Server works!"));
+app.get('/', (req, res) => {
+    res.send("Server works!");
+});
 
-app.listen(5000, () => console.log("Running on 5000")
+app.listen(5000, () => {
+    console.log("Running on 5000");
+});
