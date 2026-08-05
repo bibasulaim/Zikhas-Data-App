7127587d5340dc5dd543eea9128bfb9ab32e4df0
Habiba Abubakar Suleiman <bibasulaim@gmail.com>
2026-08-05 09:35:21 +0100

    Enable CORS

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,17 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
 
 const app = express();
 
-
 // Connect Database
 connectDB();
 
-
 // Middleware
+app.use(cors());
 app.use(express.json());
-
 
 // Routes
 const authRoutes = require("./routes/auth");
@@ -20,23 +19,19 @@ const walletRoutes = require("./routes/wallet");
 const vtuRoutes = require("./routes/vtu");
 const transactionRoutes = require("./routes/transactions");
 
-
 // API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/wallet", walletRoutes);
 app.use("/api/vtu", vtuRoutes);
 app.use("/api/transactions", transactionRoutes);
 
-
 // Test Route
 app.get("/", (req, res) => {
   res.send("Zikhas Data App Server is running smoothly 🚀");
 });
 
-
 // Server Port
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, () => {
   console.log(`Running perfectly on port ${PORT}`);
